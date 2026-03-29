0b66a9228714008604a5f920da540cc3c683ab54
Çağrı <mccatik@gmail.com>
2026-03-29 23:48:52 +0200

    project deleted

--- car-game/src/scene.js
+++
@@ -1,7 +0,0 @@
-export { createScene } from './scene/createScene.js?v=unified-20260329-22';
-
-
-
-
-
-
--- car-game/src/scene/obstacles.js
+++
@@ -1,4 +0,0 @@
-export async function addTrackObstacles() {
-  // Cones removed: keep track open for manual and waypoint driving.
-  return [];
-}
